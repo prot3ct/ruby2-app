302ff81e0b7a22cb27763f9405b096e70473a4bc
prot3ct <djorkaevhell@abv.bg>
2017-02-21 14:04:50 +0200

    Init

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
 	root 'visits#index'
+	
+	post '/articles', to: 'articles#create'
+	get '/articles/:id', to: 'articles#get_by_id'
+	get '/articles', to: 'articles#show'
+
 	get '/visits',  to: 'visits#show'
 	post '/visits', to: 'visits#create'
 end
