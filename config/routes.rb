3f6cc6da04a81ab885ed64ec4ba14b536e9dc229
prot3ct <djorkaevhell@abv.bg>
2017-02-21 16:27:57 +0200

    asdf

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 	root 'visits#index'
 	
-	post '/articles', to: 'articles#create'
-	get '/articles/id', to: 'articles#find'
-	get '/articles', to: 'articles#show'
+	post '/articles', to: 'article#create'
+	get '/articles/id', to: 'article#find'
+	get '/articles', to: 'article#show'
 
 	get '/visits',  to: 'visits#show'
 	post '/visits', to: 'visits#create'
