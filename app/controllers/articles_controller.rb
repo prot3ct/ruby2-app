717aa7a74300754eb5f16167dc09da53fac94ad4
prot3ct <djorkaevhell@abv.bg>
2017-02-21 13:58:57 +0200

    Initial

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,33 @@
+class ArticlesController < ApplicationController
+        def create 
+            current_time = Time.new
+
+            article = construct
+            article.updated_at = current_time.inspect
+            article.save
+
+            render :json => article.to_json
+        end
+
+        def get_by_id
+            article = Article.find(id: params[:id])
+            render :json => artcile.to_json            
+        end
+
+        def show
+            render :json => Article.all
+        end
+        
+        def construct 
+            if !Article.exists?(name: params[:name])
+                current_time = Time.new 
+                article = Article.new
+                article.name = params[:name]
+                article.created_at = current_time.inspect
+                article.updated_at = current_time.inspect
+                artcile.save
+            end
+            
+            return Article.find_by(name: params[:name])
+        end
+end
